a5534375f2f0a27f00b70e0c27d34179b2f41251
Kaio Eduardo <kaioeduardo2003@hotmail.com>
2023-11-28 21:13:13 -0300

    base testada

--- banco/main.c
+++ banco/main.c
@@ -3,6 +3,35 @@
 #include <math.h>
 #include <string.h>
 
+void criarTabela(){
+
+}
+
+void criarNovaLinha(){
+
+}
+
+void pesquisarDado(){
+
+}
+
+void listarTabelas(){
+
+}
+
+void listarDados(){
+
+}
+
+void apagarTabela(char nome_tabela[]){
+
+}
+
+void apagarLinha()  {
+
+}
+
+
 int main()
 {
 
@@ -11,18 +40,18 @@ int main()
     do
     {
         printf("Escolha uma opção:\n"
-               "1 ---- Criar uma nova tabela\n"
-               "2 ---- Criar uma nova linha na tabela\n"
-               "3 ---- Pesquisar valor em uma tabela\n"
-               "4 ---- Listar todas as tabelas\n"
-               "5 ---- Listar todos os dados de uma tabela\n"
-               "6 ---- Apagar uma tabela\n"
-               "7 ---- Apagar uma linha de uma tabela\n"
-               "0 ---- Sair \n"
-
+               "1 - Criar uma nova tabela\n"
+               "2 - Criar uma nova linha na tabela\n"
+               "3 - Pesquisar valor em uma tabela\n"
+               "4 - Listar todas as tabelas\n"
+               "5 - Listar todos os dados de uma tabela\n"
+               "6 - Apagar uma tabela\n"
+               "7 - Apagar uma linha de uma tabela\n"
+               "0 - Sair \n"
         );
 
         scanf("%i", &opcao);
+        char nome_tabela[20];
 
         switch (opcao)
         {
@@ -31,14 +60,17 @@ int main()
             break;
         
         case 2:
-            char nome_tabela[20]; //exemplo
+            // char nome_tabela[20]; //exemplo
+            printf("Digite o nome da tabela: ");
+            scanf("\n");
             fgets(nome_tabela, 21, stdin);
-            
             criarNovaLinha();
             break;
         
         case 3:
-            char nome_tabela[20]; //exemplo
+            // char nome_tabela[20]; //exemplo
+            printf("Digite o nome da tabela: ");
+            scanf("\n");
             fgets(nome_tabela, 21, stdin);
             
             pesquisarDado();
@@ -49,14 +81,18 @@ int main()
             break;
         
         case 5:
-            char nome_tabela[20]; //exemplo
+            // char nome_tabela[20]; //exemplo
+            printf("Digite o nome da tabela: ");
+            scanf("\n");
             fgets(nome_tabela, 21, stdin);
             
             listarDados();
             break;
         
         case 6:
-            char nome_tabela[20]; //exemplo
+            // char nome_tabela[20]; //exemplo
+            printf("Digite o nome da tabela: ");
+            scanf("\n");
             fgets(nome_tabela, 21, stdin);
             
             apagarTabela(nome_tabela);
@@ -64,11 +100,15 @@ int main()
             break;
         
         case 7:
-            char nome_tabela[20]; //exemplo
+            // char nome_tabela[20]; //exemplo
+            
             int chave_primaria;
-
+            printf("Digite o nome da tabela: ");
+            scanf("\n");
             fgets(nome_tabela, 21, stdin);
-            scanf("%i", chave_primaria);
+            
+            printf("Digite a chave primária da linha: ");
+            scanf("%i", &chave_primaria);
 
             apagarLinha();
             break;
@@ -77,7 +117,7 @@ int main()
             break;
 
         default:
-            printf("Selecione uma opção válida");
+            printf("Selecione uma opção válida\n");
             break;
         }
 
