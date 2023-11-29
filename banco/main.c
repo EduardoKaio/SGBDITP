0cff14170f6d7a2206297daf18cd5f4b0d3cf887
Felipe Freitas <felipefreitas13@live.com>
2023-11-29 18:32:54 -0300

    Criar tabela

--- banco/main.c
+++ banco/main.c
@@ -2,10 +2,68 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <errno.h>
 
-void criarTabela(){
+FILE *tabelas;
 
-}
+typedef struct
+{
+    int chave_primaria;
+    char tipo_coluna;
+    char nome_coluna[20];
+} Coluna;
+
+typedef struct
+{
+    char nome_tabela[50];
+    int n_colunas;
+} Tabela;
+
+int cont = 0;
+
+void criarTabela(Tabela tabela[cont]){
+    cont++;
+    
+    if (cont > 1){
+        tabela = realloc(tabela, cont * sizeof(int));
+    }
+
+    tabelas = fopen("tabelas.txt", "a");
+    if(tabelas == NULL){
+        printf("Ocorreu um erro ao criar a tabela");
+        printf("O erro foi: %s\n", strerror(errno));
+    } else {
+        printf("Informe o nome da tabela:");
+        scanf("%s", tabela[cont].nome_tabela);
+        fprintf(tabelas, "%s\n", tabela[cont].nome_tabela);
+        printf("Informe quantas colunas deseja inserir:");
+        scanf("%i", tabela[cont].n_colunas);
+
+        Coluna coluna[tabela[cont].n_colunas];
+
+        for(int i=0; i<tabela[cont].n_colunas; i++){
+            if (i == 0)
+            {
+                printf("informe o nome da coluna com a chave primária");
+                scanf("%s", coluna[i].nome_coluna);
+                fprintf(tabelas, "%s | ", coluna[i].nome_coluna);
+            } else {
+                
+                printf("Informe o tipo e o nome da %iº coluna que deseja criar:\n"
+                    "i para inteiro\n"
+                    "c para caractere\n"
+                    "f para float\n"
+                    "d para double\n"
+                    "s para string\n", i+1
+                );
+                
+                scanf("%c %s", &coluna[i].tipo_coluna, coluna[i].nome_coluna);
+                fprintf(tabelas, "%s | ", coluna[i].nome_coluna);
+            }
+        }
+        fclose(tabelas);
+    }
+} 
 
 void criarNovaLinha(){
 
@@ -36,6 +94,7 @@ int main()
 {
 
     int opcao;
+    Tabela tabela[cont];
 
     do
     {
@@ -56,14 +115,14 @@ int main()
         switch (opcao)
         {
         case 1:
-            criarTabela();
+            criarTabela(tabela);
             break;
         
         case 2:
             // char nome_tabela[20]; //exemplo
             printf("Digite o nome da tabela: ");
             scanf("\n");
-            fgets(nome_tabela, 21, stdin);
+            fgets(nome_tabela, 51, stdin);
             criarNovaLinha();
             break;
         
@@ -71,7 +130,7 @@ int main()
             // char nome_tabela[20]; //exemplo
             printf("Digite o nome da tabela: ");
             scanf("\n");
-            fgets(nome_tabela, 21, stdin);
+            fgets(nome_tabela, 51, stdin);
             
             pesquisarDado();
             break;
@@ -84,7 +143,7 @@ int main()
             // char nome_tabela[20]; //exemplo
             printf("Digite o nome da tabela: ");
             scanf("\n");
-            fgets(nome_tabela, 21, stdin);
+            fgets(nome_tabela, 51, stdin);
             
             listarDados();
             break;
@@ -93,7 +152,7 @@ int main()
             // char nome_tabela[20]; //exemplo
             printf("Digite o nome da tabela: ");
             scanf("\n");
-            fgets(nome_tabela, 21, stdin);
+            fgets(nome_tabela, 51, stdin);
             
             apagarTabela(nome_tabela);
             
@@ -105,7 +164,7 @@ int main()
             int chave_primaria;
             printf("Digite o nome da tabela: ");
             scanf("\n");
-            fgets(nome_tabela, 21, stdin);
+            fgets(nome_tabela, 51, stdin);
             
             printf("Digite a chave primária da linha: ");
             scanf("%i", &chave_primaria);
